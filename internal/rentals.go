78db41678d3f0de0f3ea75e17871cf5254eca5c8
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2023-09-15 08:57:33 +0300

    added parameters support

--- internal/rentals.go
+++ internal/rentals.go
@@ -1,8 +1,10 @@
 package internal
 
 import (
+	"fmt"
 	"net/url"
 	"outdoorsy-api/database"
+	"strings"
 )
 
 // GetASingleRental retrieves a single rental from the database by the specified id.
@@ -16,5 +18,87 @@ func GetMultipleRentals(params url.Values) (rentals []Rental, err error) {
 		err = database.GetMultipleRecords(&rentals, selectAllRentalsQuery)
 		return
 	}
+	additionalQueryParams := transpileParamsToDBQueries(params)
+	err = database.GetMultipleRecords(&rentals, selectAllRentalsQuery+additionalQueryParams)
 	return
+}
+
+func transpileParamsToDBQueries(params url.Values) (additionalQueryParams string) {
+	var builder strings.Builder
+
+	handleDBQueryWhereClauses(params, &builder)
+	handleDBQuerySortingClause(params, &builder)
+	handleDBQueryFinalClauses(params, &builder)
+
+	additionalQueryParams = builder.String()
+	additionalQueryParams = strings.ReplaceAll(additionalQueryParams, "[", "")
+	additionalQueryParams = strings.ReplaceAll(additionalQueryParams, "]", "")
+	return
+}
+
+func handleDBQueryWhereClauses(params url.Values, builder *strings.Builder) {
+	var (
+		queryWhereClause string
+		whereParameters  = map[string]string{
+			"price_min": " price_per_day >= %s",
+			"price_max": " price_per_day <= %s",
+			"ids":       " rentals.id IN (%s)",
+			"near":      " lat >= %s AND lng >= %s",
+		}
+	)
+
+	for key, value := range params {
+		content, contentWasFound := whereParameters[key]
+		if !contentWasFound {
+			continue
+		}
+
+		if key == "near" {
+			splitValues := strings.Split(value[0], ",")
+			queryWhereClause = fmt.Sprintf(content, splitValues[0], splitValues[1])
+		} else {
+			queryWhereClause = fmt.Sprintf(content, value)
+		}
+
+		if builder.Len() > 0 {
+			builder.WriteString(" AND ")
+			builder.WriteString(queryWhereClause)
+		} else {
+			builder.WriteString(" WHERE ")
+			builder.WriteString(queryWhereClause)
+		}
+	}
+}
+
+func handleDBQuerySortingClause(params url.Values, builder *strings.Builder) {
+	var querySortingClause string
+
+	if params.Has("sort") {
+		get := params.Get("sort")
+		if get == "price" {
+			querySortingClause = " ORDER BY price_per_day"
+		} else {
+			querySortingClause = fmt.Sprintf(" ORDER BY %s", get)
+		}
+		builder.WriteString(querySortingClause)
+	}
+}
+
+func handleDBQueryFinalClauses(params url.Values, builder *strings.Builder) {
+	var (
+		queryFinalClause      string
+		finalClauseParameters = map[string]string{
+			"limit":  " LIMIT %s",
+			"offset": " OFFSET %s",
+		}
+	)
+
+	for key, value := range params {
+		content, contentWasFound := finalClauseParameters[key]
+		if !contentWasFound {
+			continue
+		}
+		queryFinalClause = fmt.Sprintf(content, value)
+		builder.WriteString(queryFinalClause)
+	}
 }
